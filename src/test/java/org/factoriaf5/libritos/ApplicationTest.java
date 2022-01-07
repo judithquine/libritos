f5a43c597b61ddcce388dd9394c162edf0767691
judithquine <judithquine@yahoo.es>
2022-01-07 18:16:56 +0100

    added WebSecurityConfig class for security config

--- src/test/java/org/factoriaf5/libritos/ApplicationTest.java
+++ src/test/java/org/factoriaf5/libritos/ApplicationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -27,6 +29,7 @@ public class ApplicationTest {
         MockMvc mockMvc;
 
         @Test
+        @WithMockUser
         void loadsTheHomePage() throws Exception {
                 mockMvc.perform(get("/"))
                         .andExpect(status().isOk())
@@ -38,6 +41,7 @@ public class ApplicationTest {
         BookRepository bookRepository;
 
         @Test
+        @WithMockUser
         void returnsTheExistingBooks() throws Exception {
 
                 Book book = bookRepository.save(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "fantasy"));
@@ -54,6 +58,7 @@ public class ApplicationTest {
         }
 
         @Test
+        @WithMockUser
         void returnsAFormToAddNewBooks() throws Exception {
                 mockMvc.perform(get("/books/new"))
                         .andExpect(status().isOk())
@@ -64,11 +69,13 @@ public class ApplicationTest {
         }
 
         @Test
+        @WithMockUser
         void allowsToCreateANewBook() throws Exception {
                 mockMvc.perform(post("/books/new")
                                 .param("title", "Harry Potter and the Philosopher's Stone")
                                 .param("author", "J.K. Rowling")
                                 .param("category", "fantasy")
+                                .with(csrf())
                         )
                         .andExpect(status().is3xxRedirection())
                         .andExpect(redirectedUrl("/books"))
@@ -83,6 +90,7 @@ public class ApplicationTest {
         }
 
         @Test
+        @WithMockUser
         void returnsAFormToEditBooks() throws Exception {
                 Book book = bookRepository.save(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "fantasy"));
                 mockMvc.perform(get("/books/edit/" + book.getId()))
@@ -93,6 +101,7 @@ public class ApplicationTest {
         }
 
         @Test
+        @WithMockUser
         void allowsToDeleteABook() throws Exception {
                 Book book = bookRepository.save(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "fantasy"));
                 mockMvc.perform(get("/books/delete/" + book.getId()))
@@ -100,6 +109,17 @@ public class ApplicationTest {
                         .andExpect(redirectedUrl("/books"));
 
                 assertThat(bookRepository.findById(book.getId()), equalTo(Optional.empty()));
+        }
+
+        @Test
+        void anonymousUsersCannotCreateABook() throws Exception {
+                mockMvc.perform(post("/books/new")
+                                .param("title", "Harry Potter and the Philosopher's Stone")
+                                .param("author", "J.K. Rowling")
+                                .param("category", "fantasy")
+                                .with(csrf()))
+                        .andExpect(status().is3xxRedirection())
+                        .andExpect(redirectedUrl("http://localhost/login"));
         }
 
 }
